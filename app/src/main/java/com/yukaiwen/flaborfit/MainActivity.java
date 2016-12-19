1b5b3dde0e6898ae5f38a66b570764d7df49da44
amelieykw <amelieykw1991@gmail.com>
2016-12-19 21:01:46 +0100

    add the second activity, can call this second activity from the mainActivity by Intent

--- app/src/main/java/com/yukaiwen/flaborfit/MainActivity.java
+++ app/src/main/java/com/yukaiwen/flaborfit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yukaiwen.flaborfit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_ITEM_TITLE = "extra.item.title";
     public static final String EXERCISE_WEIGHTS = "Weight lifting";
     public static final String EXERCISE_YOGA = "Yoga";
     public static final String EXERCISE_CARDIO = "Cardio";
@@ -44,6 +46,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void launchDetailActivity(String exerciseTitle) {
-
+//        to tell the activity manager that it's MainActivity which loads the DetailsActivity
+        Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+//        to tell the activity manager to fetch the value exerciseTitle and then store it into the keyname EXTRA_ITEM_TITLE
+        intent.putExtra(MainActivity.EXTRA_ITEM_TITLE, exerciseTitle);
+        startActivity(intent);
     }
 }
